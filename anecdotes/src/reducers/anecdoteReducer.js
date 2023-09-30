475244bdf8bea393fa6d5419f38713a22578be90
Michael Ikoko <michaelikoko.o@gmail.com>
2023-09-30 01:40:57 +0100

    Finished anecdotes

--- anecdotes/src/reducers/anecdoteReducer.js
+++ anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,41 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE': {
+      const id = action.payload.id
+      const anecdoteToChange = state.find(n => n.id === id)
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      )
+    }
+    case 'NEW_ANECDOTE': {
+      return [...state, action.payload ]
+    }
+    default: return state
+  }
+}
+
+export const voteAnecdote = (id) => {
+  return {
+    type: 'VOTE',
+    payload: { id }
+  }
+}
+
+export const createAnecdote = (content) => {
+  return {
+    type: 'NEW_ANECDOTE',
+    payload: {
+      content,
+      id: getId(),
+      votes: 0
+    }
+  }
 }
 
 export default reducer
---
+++ anecdotes/src/reducers/anecdoteReducer.test.js
@@ -0,0 +1,56 @@
+import reducer from "./anecdoteReducer"
+import deepFreeze from "deep-freeze"
+
+describe('anecdoteReducer', () => {
+    const initialState = [
+        {
+            content: 'If it hurts, do it more often',
+            id: 1,
+            votes: 0
+        },
+        {
+            content: 'Adding manpower to a late software project makes it later!',
+            id: 2,
+            votes: 0
+        },
+        {
+            content: 'Premature optimization is the root of all evil.',
+            id: 3,
+            votes: 0
+        }
+    ]
+
+    test('VOTE is implemented', () => {
+        const state = initialState
+        const action = {
+            type: 'VOTE',
+            payload: { id: 2 }
+        }
+
+        deepFreeze(state)
+        const newState = reducer(state, action)
+
+        expect(newState).toHaveLength(3)
+        expect(newState[1]).toEqual({
+            ...state[1],
+            votes: 1         
+        })
+    })
+
+    test('NEW ANECDOTE is implemented', () => {
+        const state = initialState
+        const action = {
+            type: 'NEW_ANECDOTE',
+            payload: {
+                content: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+                id: 4,
+                votes: 0
+            }
+        }
+
+        deepFreeze(state)
+        const newState = reducer(state, action)
+        expect(newState).toHaveLength(4)
+        expect(newState).toContainEqual(action.payload)
+    })
+})
